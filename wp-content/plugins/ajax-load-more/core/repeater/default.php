fddb7f4ee6f1dcc608d67e460108f1f90665b382
zohaibmir <zohaib.mir@gmail.com>
2015-09-30 00:37:05 +0200

    update template

--- wp-content/plugins/ajax-load-more/core/repeater/default.php
+++ wp-content/plugins/ajax-load-more/core/repeater/default.php
@@ -1,47 +1,124 @@
-<article id="post-1173807" class="marquee-article large-12" data-post-id="1173807">
-                <div class="img-wrapper">
-                    <a href="<?php the_permalink(); ?>">
-                        <?php the_post_thumbnail('big-image'); ?>
-                    </a>
-                </div>
-                <header class="post-header large-12 columns">
-                    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                    <div class="entry-content">
-                        <p><?php the_content_rss(',', 0, '', 10); ?></p>
-                    </div>
-                </header>
-                <div class="sub-text large-12 columns">
-                    <?php $author = get_user_by('id', (int) $post->post_author); ?>
-                    <?php echo get_avatar($author->user_email, 40); ?>
+<?php 
+if($counter == 0 ) {
+			$style1 = true;
+                $style2 = false;
+}
 
-                    <div class="author-thumb-info">
-                        <author>
-                            <span class="author-name">
-
-                                <?php
-                                $first_name = get_user_meta((int) $post->post_author, 'first_name');
-                                $last_name = get_user_meta((int) $post->post_author, 'last_name');
-                                ?>
-                                <?php echo $first_name[0]; ?>
-                                <?php echo $last_name[0]; ?>
-                            </span>
-                        </author>
-                        <?php
-                        $tags = wp_get_post_terms($post->ID, 'post_tag', array('orderby' => 'name', 'order' => 'ASC', 'fields' => 'all'));
+ if ($counter == 3) {
+                            $style1 = false;
+                            $style2 = true;
+                        }
+ 				
+$first_name = get_user_meta((int) $post->post_author, 'first_name');
+                        $last_name = get_user_meta((int) $post->post_author, 'last_name');
                         ?>
-                        <span class="category-name"> on  <?php $tag_counter = 0; ?>
-                            <?php foreach ($tags as $tag) : ?>
-                                <?php if ($tag_counter == 0) : ?>
-                                    <a  style="text-transform: uppercase; padding:0x 15px; font-size:0.8em;font-weight:bold;" href="<?php echo get_term_link($tag->term_id, 'post_tag'); ?>">
 
-                                        <?php echo $tag->name; ?>
+                        <?php if ($style1) { ?>
+                            <article id="post-1173807" class="marquee-article large-12" data-post-id="1173807">
+                                <div class="img-wrapper">
+                                    <a href="<?php the_permalink(); ?>">
+                                        <?php the_post_thumbnail('big-image'); ?>
                                     </a>
-                                    <?php $tag_counter++; ?>
-                                <?php endif; ?>
-                            <?php endforeach; ?></span>
-                    </div>
-                    <div class="bookmark-wrapper">
-                        <a data-pid="1173807" href="#" class="icon-ed-bookmark bookmark-button-sign-in" data-ev-name="sign-in" data-ev-loc="featured-loop">Read Later</a>
-                    </div>
-                    <time class="updated icon-ed-clock" datetime="2015-08-11T19:53:19+00:00" pubdate><span class="dateStamp"> Aug 11,&nbsp; </span><span class="timeStamp">3:53pm</span></time>					</div>
- </article>
+                                </div>
+                                <header class="post-header large-12 columns">
+                                    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); echo $counter?></a></h2>
+                                    <div class="entry-content">
+                                        <p><?php //echo get('byline'); ?></p>
+                                    </div>
+                                </header>
+                                <div class="sub-text large-12 columns">
+                                    <?php $author = get_user_by('id', (int) $post->post_author); ?>
+                                    <?php echo get_avatar($author->user_email, 40); ?>
+
+                                    <div class="author-thumb-info">
+                                        <author>
+                                            <span class="author-name">
+
+
+                                                <?php echo $first_name[0]; ?>
+                                                <?php echo $last_name[0]; ?>
+                                            </span>
+                                        </author>
+                                        <?php
+                                        $tags = wp_get_post_terms($post->ID, 'post_tag', array('orderby' => 'name', 'order' => 'ASC', 'fields' => 'all'));
+                                        ?>
+                                        <span class="category-name"> on  <?php $tag_counter = 0; ?>
+                                            <?php foreach ($tags as $tag) : ?>
+                                                <?php if ($tag_counter == 0) : ?>
+                                                    <a  style="text-transform: uppercase; padding:0x 15px; font-size:0.8em;font-weight:bold;" href="<?php echo get_term_link($tag->term_id, 'post_tag'); ?>">
+
+                                                        <?php echo $tag->name; ?>
+                                                    </a>
+                                                    <?php $tag_counter++; ?>
+                                                <?php endif; ?>
+                                            <?php endforeach; ?></span>
+                                    </div>
+                                <time class="updated"><i class="gen-enclosed fi-clock"></i> <?php echo  get_the_time('l, F jS, Y'); ?></time>						  
+                                </div>
+                            </article>
+
+                            <?php
+                        }
+                        $counter++;
+                       
+
+
+                        if ($style2) {
+                            ?>
+
+                            <article id="post-1224581" class="horizontal-article hz-img-left large-12" data-post-id="1224581">
+                                <div class="img-wrapper large-5 medium-5 left">
+                                    <a href="<?php the_permalink(); ?>">
+                                        <?php the_post_thumbnail('big-image'); ?>
+                                    </a>
+                                </div>
+                                <div class="horizontal-post-info large-7 medium-7 columns">
+                                    <header class="post-header">
+                                        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); echo $counter ?></a></h2>
+                                        <div class="custom-article-excerpt"><span>
+                                        <?php //echo get('byline'); ?></span></div>						</header>
+                                    <div class="sub-text large-12 columns">
+                                        <a class="card-author-avatar-wrapper" href="#">
+                                            <?php $author = get_user_by('id', (int) $post->post_author); ?>
+                                            <?php echo get_avatar($author->user_email, 40); ?>                                              
+                                        </a>
+                                        <div class="author-thumb-info">
+                                            <author>
+                                                <span class="author-name">
+                                                    <a href="#">
+                                                        <?php
+                                                        echo $first_name[0] . ' ' . $last_name[0];
+                                                        ?>
+                                                    </a>																			</span>
+                                            </author>
+                                            <span class="category-name"> on 
+                                                <a href="#">
+                                                    <?php
+                                                    $tags = wp_get_post_terms($post->ID, 'post_tag', array('orderby' => 'name', 'order' => 'ASC', 'fields' => 'all'));
+                                                    ?>
+                                                    
+                                                        <?php foreach ($tags as $tag) : ?>
+                                                            <?php if ($tag_counter == 0) : ?>
+                                                                <a  style="text-transform: uppercase; padding:0x 15px; font-size:0.8em;font-weight:bold;" href="<?php echo get_term_link($tag->term_id, 'post_tag'); ?>">
+
+                                                                    <?php echo $tag->name; ?>
+                                                                </a>
+                                                                <?php $tag_counter++; ?>
+                                                            <?php endif; ?>
+                                                        <?php endforeach; ?></span>
+                                                </a>
+                                            </span>
+                                        </div>
+                                        
+                                       <time class="updated"><i class="gen-enclosed fi-clock"></i><?php echo  get_the_time('l, F jS, Y'); ?></time>						
+                                    </div>
+                                </div>
+                            </article>
+                            <?php
+                        }
+                        if ($counter == 6) {
+                            $style1 = true;
+                            $style2 = false;
+                            $counter = 0;
+                        }
+                        ?>
